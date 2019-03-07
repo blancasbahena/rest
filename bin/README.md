fa6e6fbc07bce2aff9dc2b64ac2e55ce61ad5e8b
Domingo Fernández <domingo.fernandez@stratplus.net>
2019-03-07 13:32:36 -0600

    lblancas

--- bin/README.md
+++
@@ -1,2 +0,0 @@
-# TKN-API-MBSS
-This project holds the business methods related to the MBSS biometric system. Other biometric system should implement their own logic
--- bin/script_init.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-sleep 5s
-until wget http://config:8001/application/application -O application.out; do
-	echo "Config server is unavailable"
-	sleep 1
-done
-echo "Config server is up and running"
-java -Xms256m -Xmx512m -XX:-TieredCompilation -Xss256k -XX:+UseG1GC -XX:+UseStringDeduplication -Djava.security.egd=file:/dev/./urandom -jar /home/tkn-api-mbss.jar
